4981a2e8cdd6d30d412d454b7f1b83a963a557f3
choonkeun <choonkeun@yahoo.com>
2018-08-31 17:59:24 -0700

    Add

---
+++ ThreadApp2-ThreadPool/ThreadApp2/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace ThreadApp2_ThreadPool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnThreadPool1 = new System.Windows.Forms.Button();
+            this.rtbResults = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnThreadPool2 = new System.Windows.Forms.Button();
+            this.btnThreadPool3 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // btnThreadPool1
+            // 
+            this.btnThreadPool1.Location = new System.Drawing.Point(12, 12);
+            this.btnThreadPool1.Name = "btnThreadPool1";
+            this.btnThreadPool1.Size = new System.Drawing.Size(306, 29);
+            this.btnThreadPool1.TabIndex = 0;
+            this.btnThreadPool1.Text = "ThreadPool 1 Start - Simple Parameter";
+            this.btnThreadPool1.UseVisualStyleBackColor = true;
+            this.btnThreadPool1.Click += new System.EventHandler(this.btnThreadPool1_Click);
+            // 
+            // rtbResults
+            // 
+            this.rtbResults.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtbResults.Location = new System.Drawing.Point(324, 47);
+            this.rtbResults.Name = "rtbResults";
+            this.rtbResults.Size = new System.Drawing.Size(273, 203);
+            this.rtbResults.TabIndex = 1;
+            this.rtbResults.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(325, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 19);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // btnThreadPool2
+            // 
+            this.btnThreadPool2.Location = new System.Drawing.Point(12, 47);
+            this.btnThreadPool2.Name = "btnThreadPool2";
+            this.btnThreadPool2.Size = new System.Drawing.Size(306, 29);
+            this.btnThreadPool2.TabIndex = 3;
+            this.btnThreadPool2.Text = "ThreadPool 2 Start - Class as parameter";
+            this.btnThreadPool2.UseVisualStyleBackColor = true;
+            this.btnThreadPool2.Click += new System.EventHandler(this.btnThreadPool2_Click);
+            // 
+            // btnThreadPool3
+            // 
+            this.btnThreadPool3.Location = new System.Drawing.Point(12, 82);
+            this.btnThreadPool3.Name = "btnThreadPool3";
+            this.btnThreadPool3.Size = new System.Drawing.Size(306, 29);
+            this.btnThreadPool3.TabIndex = 4;
+            this.btnThreadPool3.Text = "ThreadPool 3 Start - Form Progressbar Display";
+            this.btnThreadPool3.UseVisualStyleBackColor = true;
+            this.btnThreadPool3.Click += new System.EventHandler(this.btnThreadPool3_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 256);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(585, 23);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(609, 288);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnThreadPool3);
+            this.Controls.Add(this.btnThreadPool2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rtbResults);
+            this.Controls.Add(this.btnThreadPool1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "ThreadApp2-ThreadPool";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnThreadPool1;
+        private System.Windows.Forms.RichTextBox rtbResults;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnThreadPool2;
+        private System.Windows.Forms.Button btnThreadPool3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ ThreadApp2-ThreadPool/ThreadApp2/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ThreadApp2_ThreadPool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //1. All ThreadPool threads are background threads
+        //2. All ThreadPool threads are in the multithreaded apartment
+
+        #region btnThreadPool1 - console
+        private void btnThreadPool1_Click(object sender, EventArgs e)
+        {
+            rtbResults.Text = string.Empty;
+            label1.Text = string.Empty;
+
+            ThreadPool.SetMaxThreads(1, 5);     //set minimum and maximum ThreadPool Count
+
+            for (int i = 0; i < 100; i++)
+                ThreadPool.QueueUserWorkItem(new WaitCallback(TaskCallBack), i);    //Threads are auto Start and reused
+
+            Console.WriteLine("Done...");
+        }
+
+        //Thread Method
+        private void TaskCallBack(Object state)
+        {
+            string ThreadName = "Thread " + state.ToString();
+
+            for (int i = 0; i < 5; i++)
+                Console.WriteLine(ThreadName + ": " + i.ToString());
+
+            Console.WriteLine(ThreadName + " Finished...");
+        }
+        #endregion
+
+        #region btnThreadPool2 - Class as Thread parameter
+
+        public delegate void LabelDelegate(string state);
+
+        class ThreadInfo2
+        {
+            public LabelDelegate FuncName { get; set; }
+            public int SelectedIndex { get; set; }
+        }
+
+        private void btnThreadPool2_Click(object sender, EventArgs e)
+        {
+            ThreadInfo2 threadInfo = new ThreadInfo2();
+            threadInfo.FuncName = UpdateLabel;
+            threadInfo.SelectedIndex = 3;
+
+            ThreadPool.QueueUserWorkItem(new WaitCallback(ProcessFile2), threadInfo);   //1 thread
+            System.Threading.Thread.Sleep(1000);
+            label1.Text = "Done";
+        }
+
+        //Thread Method
+        private void ProcessFile2(object state)
+        {
+            ThreadInfo2 threadInfo = state as ThreadInfo2;
+            LabelDelegate FuncName = threadInfo.FuncName;
+            int index = threadInfo.SelectedIndex;
+
+            //this.Invoke(new LabelDelegate(UpdateLabel));        //Invoke delegate function
+            FuncName.Invoke("ProcessFile2");
+        }
+
+        //update Form1.Lable
+        private void UpdateLabel(string state)
+        {
+            label1.BeginInvoke((MethodInvoker)delegate
+            {
+                //label1.Text = state;
+                rtbResults.Text = state;
+            });
+            System.Threading.Thread.Sleep(1000);
+        }
+
+        #endregion
+
+        #region btnThreadPool3 - Form ProgressBar Display
+
+        public delegate void BarDelegate();
+
+        class ThreadInfo3
+        {
+            public int index { get; set; }
+        }
+
+        private void btnThreadPool3_Click(object sender, EventArgs e)
+        {
+            progressBar1.Maximum = 100;
+            progressBar1.Minimum = 0;
+
+            ThreadInfo3 threadInfo = new ThreadInfo3();
+            threadInfo.index = 70;
+
+            ThreadPool.QueueUserWorkItem(new WaitCallback(ProcessFile3), threadInfo);   //1 thread
+            MessageBox.Show("Done");
+        }
+
+        //Thread Method
+        private void ProcessFile3(object state)
+        {
+            ThreadInfo3 threadInfo = state as ThreadInfo3;
+            int index = threadInfo.index;
+
+            //UI Update
+            try
+            {
+                for (int i = 0; i < index; i++)
+                {
+                    this.Invoke(new BarDelegate(UpdateBar));        //Invoke delegate function
+
+                    //label1.Text = i.ToString();
+                    //Cross-thread operation not valid: Control 'label1' accessed from a thread other than the thread it was created on.
+                    //'ProcessFile3' is Thread callback
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        //Update the graphical bar: delegate is belong to Form1
+        private void UpdateBar()
+        {
+            progressBar1.Value++;
+
+            rtbResults.Text += progressBar1.Value.ToString() + "\n";
+
+            if (progressBar1.Value == progressBar1.Maximum)
+            {
+                // We are finished and the progress bar is full.
+                //return true;
+            }
+        }
+
+        #endregion
+
+
+
+    }
+}
